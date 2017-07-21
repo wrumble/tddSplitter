3cb5b7af127ac0ab907ec09705a05520e14fd229
Wayne Rumble <rumblewayne@gmail.com>
2017-07-21 12:20:15 +0100

    set up for cloudkit

---
+++ Splitter/Splitter/WelcomeScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeScreenViewController.swift
+//  Splitter
+//
+//  Created by Wayne Rumble on 21/07/2017.
+//  Copyright © 2017 Wayne Rumble. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeScreenViewController: UIViewController {
+
+}
