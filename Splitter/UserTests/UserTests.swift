d5b542f113a2e6da5c220ee8b639542685714c0d
Wayne Rumble <rumblewayne@gmail.com>
2017-07-27 17:59:33 +0100

    add passing signui and signout test

--- Splitter/UserTests/UserTests.swift
+++ Splitter/UserTests/UserTests.swift
@@ -15,6 +15,7 @@ class UserTests: XCTestCase {
     
     let databaseReference = Database.database().reference()
     let firebaseData = FirebaseData()
+    var user: User?
     
     override func setUp() {
         super.setUp()
@@ -25,14 +26,12 @@ class UserTests: XCTestCase {
     }
     
     func testCanCreateUser() {
-        var testSuccess = true
-        var user = SplitterUser(name: "Bob Ross", email: "test@email.com")
+        var testSuccess = false
+        let user = SplitterUser(name: "Bob Ross", email: "test@email.com")
         let requestExpectation = expectation(description: "Creates a user")
         
         firebaseData.createUser(email: user.email, password: "password", completion: { (error, firebaseUser) in
             if let firebaseUser = firebaseUser {
-                user.id = firebaseUser.uid
-                print(user.id)
                 testSuccess = true
                 self.removeUser(firebaseUser)
             } else {
@@ -43,6 +42,58 @@ class UserTests: XCTestCase {
         waitForExpectations(timeout: 10) { (error) in
             XCTAssertTrue(testSuccess)
         }
+    }
+    
+    func testUserCanLoginUser() {
+        createUser()
+        var testSuccess = false
+        let requestExpectation = expectation(description: "Creates a user")
+        
+        firebaseData.signInUser(email: "test@email.com", password: "password", completion: { (error, firebaseUser) in
+            if let firebaseUser = firebaseUser {
+                testSuccess = true
+                self.removeUser(firebaseUser)
+            } else {
+                XCTFail(String(describing: error!))
+            }
+            requestExpectation.fulfill()
+        })
+        waitForExpectations(timeout: 10) { (error) in
+            XCTAssertTrue(testSuccess)
+        }
+        
+    }
+    
+    func testUserCanLogout() {
+        createUser()
+        var testSuccess = false
+        let requestExpectation = expectation(description: "Creates a user")
+        
+        firebaseData.signOutUser(completion: { (signedOut) in
+            testSuccess = signedOut
+            if self.user != nil {
+                self.removeUser(self.user!)
+            }
+            requestExpectation.fulfill()
+        })
+        waitForExpectations(timeout: 10) { (error) in
+            XCTAssertTrue(testSuccess)
+        }
+    }
+    
+    func createUser() {
+        let user = SplitterUser(name: "Bob Ross", email: "test@email.com")
+        weak var requestExpectation = expectation(description: "Creates a user")
+        
+        firebaseData.createUser(email: user.email, password: "password", completion: { (error, firebaseUser) in
+            if let error = error {
+                print(error)
+            } else {
+                self.user = firebaseUser
+            }
+            requestExpectation?.fulfill()
+        })
+        waitForExpectations(timeout: 10)
     }
     
     func removeUser(_ user: User) {
