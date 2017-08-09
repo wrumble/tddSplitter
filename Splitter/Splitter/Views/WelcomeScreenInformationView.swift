e899943118a22e6d5989478bdd936cd776dd52fd
Wayne Rumble <rumblewayne@gmail.com>
2017-08-09 18:15:12 +0100

    refactor and fix moving buttons

---
+++ Splitter/Splitter/Views/WelcomeScreenInformationView.swift
@@ -0,0 +1,169 @@
+//
+//  WelcomeScreenInformationView.swift
+//  Splitter
+//
+//  Created by Wayne Rumble on 09/08/2017.
+//  Copyright © 2017 Wayne Rumble. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeScreenInformationView: UIView {
+
+    // swiftlint:disable line_length
+    private let titleLogoLabel = TitleLabelLogo(frame: CGRect.zero, accessID: AccesID.titleLogoLabel)
+    private let emailTextField = SplitterTextField(frame: CGRect.zero, accessID: AccesID.emailTextField)
+    private let passwordTextField = SplitterTextField(frame: CGRect.zero, accessID: AccesID.passwordTextField)
+    private let confirmPasswordTextField = SplitterTextField(frame: CGRect.zero, accessID: AccesID.confirmPasswordTextField)
+    private let loginButton = SplitterButton(frame: CGRect.zero, accessID: AccesID.loginButton)
+    private let registerButton = SplitterButton(frame: CGRect.zero, accessID: AccesID.registerButton)
+    private var emailFieldConstraint: NSLayoutConstraint?
+    private var loginButtonConstraint: NSLayoutConstraint?
+    // swiftlint:enable line_length
+    
+    var onLogin: ((String, String) -> Void)?
+    var onRegister: ((String, String, String) -> Void)?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupHierarchy()
+        setupViews()
+        setupLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupHierarchy()
+        setupViews()
+        setupLayout()
+    }
+    
+   private func setupHierarchy() {
+        addSubview(titleLogoLabel)
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(confirmPasswordTextField)
+        addSubview(loginButton)
+        addSubview(registerButton)
+    }
+    
+    private func setupViews() {
+        passwordTextField.isSecureTextEntry = true
+
+        confirmPasswordTextField.isSecureTextEntry = true
+        confirmPasswordTextField.isHidden = true
+        
+        loginButton.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        registerButton.addTarget(self, action: #selector(registerButtonTapped), for: .touchUpInside)
+    }
+    
+    private func setupLayout() {
+        subviews.forEach { subview in
+            subview.pinToSuperview(edges: [.left, .right])
+            subview.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        titleLogoLabel.pinTop(to: self,
+                              constant: Layout.spacer,
+                              priority: .required,
+                              relatedBy: .greaterThanOrEqual)
+        titleLogoLabel.addHeightConstraint(with: Layout.titleLogoTextHeight)
+        titleLogoLabel.pinBottom(to: emailTextField,
+                                 anchor: .top,
+                                 constant: -Layout.spacer,
+                                 priority: .required,
+                                 relatedBy: .equal)
+        
+        emailFieldConstraint = emailTextField.centerYToSuperview()
+        emailFieldConstraint?.isActive = true
+        emailTextField.addHeightConstraint(with: Layout.textFieldHeight)
+        
+        passwordTextField.pinTop(to: emailTextField,
+                                 anchor: .bottom,
+                                 constant: Layout.spacer,
+                                 priority: .required,
+                                 relatedBy: .equal)
+        passwordTextField.addHeightConstraint(with: Layout.textFieldHeight)
+        
+        confirmPasswordTextField.pinTop(to: passwordTextField,
+                                        anchor: .bottom,
+                                        constant: Layout.spacer,
+                                        priority: .required,
+                                        relatedBy: .equal)
+        confirmPasswordTextField.addHeightConstraint(with: Layout.textFieldHeight)
+        
+        loginButtonConstraint = loginButton.pinTop(to: passwordTextField,
+                           anchor: .bottom,
+                           constant: Layout.spacer,
+                           priority: .required,
+                           relatedBy: .equal)
+        loginButton.addHeightConstraint(with: Layout.buttonHeight)
+        
+        registerButton.pinTop(to: loginButton,
+                              anchor: .bottom,
+                              constant: Layout.spacer,
+                              priority: .required,
+                              relatedBy: .equal)
+        registerButton.addHeightConstraint(with: Layout.buttonHeight)
+    }
+
+    @objc private func registerButtonTapped() {
+        if confirmPasswordTextField.isHidden {
+            moveLoginButtonDown()
+        } else {
+            registerNewUer()
+        }
+    }
+    
+    private func moveLoginButtonDown() {
+        NSLayoutConstraint.deactivate([loginButtonConstraint!])
+        loginButtonConstraint = loginButton.pinTop(to: confirmPasswordTextField,
+                                                   anchor: .bottom,
+                                                   constant: Layout.spacer,
+                                                   priority: .required,
+                                                   relatedBy: .equal)
+        confirmPasswordTextField.isHidden = false
+    }
+    
+    private func registerNewUer() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        guard let confirmPassword = confirmPasswordTextField.text else { return }
+        onRegister?(email, password, confirmPassword)
+    }
+    
+    @objc private func loginButtonTapped() {
+        if !confirmPasswordTextField.isHidden {
+            moveLoginButtonUp()
+        } else {
+            loginUser()
+        }
+    }
+    
+    private func moveLoginButtonUp() {
+        NSLayoutConstraint.deactivate([loginButtonConstraint!])
+        loginButtonConstraint = loginButton.pinTop(to: passwordTextField,
+                                                   anchor: .bottom,
+                                                   constant: Layout.spacer,
+                                                   priority: .required,
+                                                   relatedBy: .equal)
+        
+        confirmPasswordTextField.isHidden = true
+    }
+    
+    private func loginUser() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        onLogin?(email, password)
+    }
+    
+    func moveViewUpWithKeyboard(height: CGFloat) {
+        NSLayoutConstraint.deactivate([emailFieldConstraint!])
+        emailFieldConstraint = emailTextField.centerYToSuperview(withConstant: -height/2, priority: .required, relatedBy: .equal)
+    }
+    
+    func moveViewDownWithKeyboard() {
+        NSLayoutConstraint.deactivate([emailFieldConstraint!])
+        emailFieldConstraint = emailTextField.centerYToSuperview()
+    }
+}
