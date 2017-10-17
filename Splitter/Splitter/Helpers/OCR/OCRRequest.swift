ac17d38af45098887239d65e1425ef30aa872462
Wayne Rumble <rumblewayne@gmail.com>
2017-10-17 11:24:12 +0100

    working ocr request

--- Splitter/Splitter/Helpers/OCR/OCRRequest.swift
+++ Splitter/Splitter/Helpers/OCR/OCRRequest.swift
@@ -11,45 +11,31 @@ import AFNetworking
 class OCRRequest {
     
     var urlExtension = "extractText"
-    
-    func uploadReceiptImage(image: String) {
-        let params = ["image": image] as [String: Any]
-        HttpRequest().post(params: params,
-                           URLExtension: urlExtension,
-                           success: { response in print(response)},
-                           fail: { response in print(response)})
-//        HttpRequest().postWithImageData(params: params,
-//                                        URLExtension: urlExtension,
-//                                        imageData: image,
-//                                        success: { response in print(response)},
-//                                        fail: { response in print(response)})
-    }
-}
-    
-class HttpRequest {
-    
     let manager = AFHTTPSessionManager()
-    let baseURL = "http://localhost:8080/"
+    let url = "https://tesseractimageserver.herokuapp.com/extractText"
     
     init() {
         setSerializers()
     }
     
-    //Set Session managers serializers.
+    func uploadReceiptImage(image: String) {
+        let params = ["image": image] as [String: Any]
+        post(params: params,
+             success: { response in print(response)},
+             fail: { response in print(response)})
+
+    }
+    
     func setSerializers() {
         manager.requestSerializer = AFHTTPRequestSerializer()
         manager.responseSerializer = AFHTTPResponseSerializer()
     }
     
-    //Add extension to baseURL
-    func setURL(URLExtension: String) -> String {
-        return baseURL + URLExtension
-    }
-    
-    //Make standard api request with params.
-    func post(params: [String: Any], URLExtension: String, success:@escaping ((String) -> Void), fail:@escaping (([String: Any]) -> Void)) {
+    func post(params: [String: Any],
+              success:@escaping ((String) -> Void),
+              fail:@escaping (([String: Any]) -> Void)) {
         
-        manager.post(self.setURL(URLExtension: URLExtension), parameters: params, progress: nil,
+        manager.post(url, parameters: params, progress: nil,
                      success: {(_ task: URLSessionDataTask, _ responseObject: Any) -> Void in
                         do {
                             let response = NSString(data: (responseObject as! NSData) as Data,
@@ -67,8 +53,12 @@ class HttpRequest {
     }
     
     func handleError(_ error: NSError) -> UIAlertController {
-        let alert = UIAlertController(title: "Please Try Again", message: error.localizedDescription, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        let alert = UIAlertController(title: "Please Try Again",
+                                      message: error.localizedDescription,
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .cancel,
+                                      handler: nil))
         return alert
     }
 }
